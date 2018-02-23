50d192cab541bec82ad871d827a480295f27eb4b
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-02-23 09:16:42 +0000

    Requested fixes for user roles checkpoint as well as a bug fix on the application view

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -7,7 +7,7 @@ class ApplicationPolicy
   end
 
   def index?
-    false
+    
   end
 
   def show?
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -5,10 +5,20 @@ class WikiPolicy < ApplicationPolicy
       scope.where(published)
     end
     
-    def new? ; user.present? ; end
-    def create? ; user.present? ; end
+    def new? 
+      create?
+    end
     
-    def update? ; user.present? ; end
-    def destroy? ; user.present? ; end
+    def create? 
+      user.present?
+    end
+    
+    def update?
+      user.present? 
+    end
+    
+    def destroy? 
+      user.present?
+    end
   end
 end
