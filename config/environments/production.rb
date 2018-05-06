dfc24a82a3d0de21b5afc702b18c6cd4857afd29
EC2 Default User <ec2-user@ip-172-31-65-99.ec2.internal>
2018-05-06 02:42:11 +0000

    another attempt to fix heroku part 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  onfig.action_mailer.default_url_options = { :host => 'https://rubypedia-lcuba.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://rubypedia-lcuba.herokuapp.com/' }
 end
